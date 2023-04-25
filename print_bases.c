23471351d26fe020adb96587aebd4f4ffd7f69a0
elbatouri <elbatouri@gmail.com>
2023-04-24 18:20:42 -0700

    final push

--- print_bases.c
+++ print_bases.c
@@ -4,8 +4,7 @@
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
-  if a flag is passed to _printf
+ * @f: pointer to the struct flags who determine if a specifier is passed
  * Return: the number of char printed
  */
 int print_hex(va_list l, flags_t *f)
